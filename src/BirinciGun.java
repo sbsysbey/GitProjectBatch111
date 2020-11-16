0ce62a77dd900591903cc952a0d0216acbd32a5a
S.Koru <skoru1972@gmail.com>
2020-11-16 22:01:35 +0100

    ikinci commit

--- src/BirinciGun.java
+++ src/BirinciGun.java
@@ -1,6 +1,7 @@
 
 public class BirinciGun {
 	public static void main(String[] args) {
-		System.out.println("11:05 - Birinci Gün");
+		System.out.println("11:57 - Birinci Gün");
+		System.out.println("ikinci commit");
 	}
 }
