f4eb4cc17425b0490168fe071bb29c4b463f7537
S.Koru <skoru1972@gmail.com>
2020-11-17 21:04:19 +0100

    Update BirinciGun

--- src/BirinciGun.java
+++ src/BirinciGun.java
@@ -1,9 +1,9 @@
 
 public class BirinciGun {
 	public static void main(String[] args) {
-		System.out.println("11:57 - Birinci Gün");
-		System.out.println("ikinci commit");
-		System.out.println("10:11 - deneme");
-		System.out.println("10:11 - ikinci gün");
+			System.out.println("11:57 - Birinci Gün");
+			System.out.println("ikinci commit");
+			System.out.println("10:11 - deneme");
+			System.out.println("10:11 - ikinci gün");
 			}
 }
