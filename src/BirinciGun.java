41ac9c4cc1fafba2cf96360507a0464624bef88b
S.Koru <skoru1972@gmail.com>
2020-11-17 20:28:23 +0100

    ikinci gun ikinci

--- src/BirinciGun.java
+++ src/BirinciGun.java
@@ -1,9 +1,8 @@
 
 public class BirinciGun {
 	public static void main(String[] args) {
-		System.out.println("11:57 - Birinci Gün");
+		System.out.println("11:57 - Birinci G�n");
 		System.out.println("ikinci commit");
-		
-		System.out.println("10:11 - ikinci gün");
+		System.out.println("ucuncu commit");
 	}
 }
